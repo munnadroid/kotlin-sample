c93ca70225e3f5157f1063c43371534469c8d590
munnadroid <gotameraju2@gmail.com>
2017-07-11 09:28:54 +0545

    login form field validation added in login activity

--- app/src/main/java/com/app/kotlinsample/LoginActivity.kt
+++ app/src/main/java/com/app/kotlinsample/LoginActivity.kt
@@ -1,13 +1,58 @@
 package com.app.kotlinsample
 
-import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import butterknife.BindView
+import butterknife.OnClick
+import butterknife.OnEditorAction
+import butterknife.OnFocusChange
 import com.app.kotlinsample.base.BaseActivity
+import com.mobsandgeeks.saripaar.annotation.Length
+import com.mobsandgeeks.saripaar.annotation.NotEmpty
+import com.mobsandgeeks.saripaar.annotation.Order
+import org.jetbrains.anko.toast
 
 class LoginActivity : BaseActivity() {
 
     override val layoutId = R.layout.activity_login
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    @NotEmpty(messageResId = R.string.username_not_empty)
+    @Order(1)
+    @BindView(R.id.usernameEditText)
+    lateinit var usernameEditText: EditText
+
+    @NotEmpty(messageResId = R.string.password_not_empty)
+    @Order(2)
+    @Length(min = 6)
+    @BindView(R.id.passwordEditText)
+    lateinit var passwordEditText: EditText
+
+
+    @OnClick(R.id.submitButton)
+    fun submitBtnClicked() {
+        validateForm()
+    }
+
+    @OnEditorAction(R.id.passwordEditText)
+    fun passportIcDoneKeyboard(actionId: Int): Boolean {
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            validateForm()
+            return true
+        }
+
+        return false
+    }
+
+    @OnFocusChange(R.id.usernameEditText, R.id.passwordEditText)
+    fun onFocusChanged(view: View, hasFocus: Boolean) {
+        if (hasFocus)
+            mValidator?.validateBefore(view)
+    }
+
+    override fun onValidationSucceeded() {
+        super.onValidationSucceeded()
+        //send data to server
+        toast("Validation success!!!")
     }
 }
